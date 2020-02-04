015f26862b7af3d7436987002a48733a94397f16
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2020-02-04 14:09:35 +0900

    initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+gem "puma"
+gem "sinatra"
+gem 'sinatra-activerecord'
+gem 'sqlite3'
---
+++ app.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+require 'sinatra/activerecord'
+configure { set :server, :puma }
+
+ActiveRecord::Base.establish_connection(
+    adapter: 'sqlite3',
+    database: './sample.db'
+)
+
+class User < ActiveRecord::Base
+end
+
+get '/' do
+  { count: User.count }.to_json
+end
+
+get '/name' do
+  User.find_by(name: params['user_name'], published: true).to_json
+end
